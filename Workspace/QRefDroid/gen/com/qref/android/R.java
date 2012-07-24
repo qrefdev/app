2e6c254ab97f9e75b820eaacc20b7817cdac55b3
adminnak@SECURE-STREAM <adminnak@SECURE-STREAM@mycompany.com>
2012-07-24 14:51:57 +0000

    git-svn-id: https://svn.secure-stream.net/svn/qref/trunk@3 d2b68217-e33a-f34f-be58-e2ef09962382

--- Workspace/QRefDroid/gen/com/qref/android/R.java
+++ Workspace/QRefDroid/gen/com/qref/android/R.java
@@ -15,8 +15,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070001;
-        public static final int webView1=0x7f070000;
+        public static final int mainWebView=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
