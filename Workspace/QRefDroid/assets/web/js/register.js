7afb39fbc008d8313184d086df5d6faaabe0c59b
nklick@SECURE-STREAM <nklick@SECURE-STREAM@mycompany.com>
2012-08-23 23:16:15 +0000

    git-svn-id: https://svn.secure-stream.net/svn/qref/trunk@16 d2b68217-e33a-f34f-be58-e2ef09962382

---
+++ Workspace/QRefDroid/assets/web/js/register.js
@@ -0,0 +1,59 @@
+function Register() {
+	var register = { "mode":"rpc", "userName": $("#emailRegister").val(), "password": $("#passwordRegister").val() }
+	
+	loader.show();
+	
+	if($("#passwordRegister").val() == "")
+	{
+		var dialogbox = new Dialog("#infobox", "Please fill out all fields");
+		dialogbox.show();
+		loader.hide();
+		return;
+	}
+	else if($("#passwordRegister").val() != $("#confirmPassword").val())
+	{
+		var dialogbox = new Dialog("#infobox", "Password and Confirm Password do not match");
+		dialogbox.show();
+		loader.hide();
+		return; 
+	}
+	
+	$.ajax({
+		type: "post",
+		data: register,
+		url: host + "services/rpc/auth/registerAccount",
+		success: function(data) {
+			var response = data;
+			
+			loader.hide();
+			
+			if(response.success == true)
+			{
+				var dialog = new Dialog("#infobox", "Registration Successful!", function() {
+				
+					Navigation.go("signin");
+				});
+				
+				dialog.show();
+			}
+			else
+			{
+				if(response.returnValue == 2)
+				{
+					var dialog = new Dialog("#infobox", "An account already exists with the provided email.");
+					dialog.show();
+				}
+				else
+				{
+					var dialog = new Dialog("#infobox", "Server Error. Please try again.");
+					dialog.show();
+				}
+			}	
+		},
+		error: function() {
+			loader.hide();
+			var dialog = new Dialog("#infobox", "Cannot connect to server");
+			dialog.show();
+		}
+	});
+}
---
+++ Workspace/QRefDroid/assets/web/js/signin.js
@@ -0,0 +1,34 @@
+function Signin() {
+	var signin = { "mode":"rpc", "userName": $("#email").val(), "password": $("#password").val() }
+	
+	loader.show();
+	
+	$.ajax({
+		type: "post",
+		data: signin,
+		dataType: "json",
+		url: host + "services/rpc/auth/login",
+		success: function(data) {
+			var response = data;
+			
+			loader.hide();
+			
+			if(response.success == true)
+			{
+				token = response.returnValue;
+				$.cookie.setCookie("QrefAuth", token, 30);
+				Navigation.go("dashboard");
+			}
+			else
+			{
+				var dialog = new Dialog("#infobox", "Invalid email or password");
+				dialog.show();
+			}	
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			loader.hide();
+			var dialog = new Dialog("#infobox", "Cannot connect to server: " + jqXHR.statusText);
+			dialog.show();
+		}
+	});
+}
