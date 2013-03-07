5221fbb7815e79240eb9e7b33126ebcd37ca6080
adminnak@SECURE-STREAM <adminnak@SECURE-STREAM@mycompany.com>
2013-03-07 04:17:43 +0000

    git-svn-id: https://svn.secure-stream.net/svn/qref/trunk@80 d2b68217-e33a-f34f-be58-e2ef09962382

--- Workspace/QRef/NodeServer/qrefdb.js
+++ Workspace/QRef/NodeServer/qrefdb.js
@@ -61,11 +61,43 @@ if (cluster.isMaster) {
 
 	var express = require('express'),
 		Router = require('./lib/router/Router'),
-		UserAuth = require('./lib/security/UserAuth');
+		UserAuth = require('./lib/security/UserAuth'),
+		http = require('http'),
+		https = require('https'),
+		fs = require('fs');
 	
 	var QRefDatabase = require('./lib/db/QRefDatabase');
+
+	var secureOptions = {
+                key: fs.readFileSync('/storage/ssl/my.qref.com.key'),
+                cert: fs.readFileSync('/storage/ssl/my.qref.com.crt')
+        };
 	
-	var app = module.exports = express.createServer();
+
+	var httpApp = express.createServer();
+	var httpsApp = express.createServer(secureOptions);
+
+	var enforceSecure = function (req, res, next) {
+		if (!req.secure) {
+			var url  = 'https://' + req.headers.host + req.path;
+			var querySet = Object.keys(req.query);
+			var queryString = '';
+			
+			if (querySet.length > 0) {
+				for (var i = 0; i < querySet.length; i++) {
+					var v = req.query[querySet[i]];
+					var k = querySet[i];
+
+					queryString += k + '=' + encodeURIComponent(v) + '&';
+				}
+
+				queryString = queryString.substr(0, queryString.length - 1);
+				url += '?' + queryString;
+			}
+
+			res.redirect(url);
+		}
+	};
 	
 	var allowCrossDomain = function(req, res, next) {
 	    res.header('Access-Control-Allow-Origin', '*');
@@ -76,31 +108,53 @@ if (cluster.isMaster) {
 	};	
 	// Configuration
 	
-	app.configure(function(){
-	  app.use(express.bodyParser({ keepExtensions: true }));
-	  app.use(express.methodOverride());
-	  app.use(express.compress());
-	  app.use(express.static('../WebContent'));
-	  app.use(allowCrossDomain);
-	  app.use(express.cookieParser());
+	httpApp.configure(function(){
+	  httpApp.use(enforceSecure);
+	  httpApp.use(express.bodyParser({ keepExtensions: true }));
+	  httpApp.use(express.methodOverride());
+	  httpApp.use(express.compress());
+	  httpApp.use(express.static('../WebContent'));
+	  httpApp.use(allowCrossDomain);
+	  httpApp.use(express.cookieParser());
 	});
 	
-	app.configure('development', function(){
-	  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	httpApp.configure('development', function(){
+	  httpApp.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 	});
 	
-	app.configure('production', function(){
-	  app.use(express.errorHandler());
+	httpApp.configure('production', function(){
+	  httpApp.use(express.errorHandler());
 	});
+
+        httpsApp.configure(function(){
+          httpsApp.use(express.bodyParser({ keepExtensions: true }));
+          httpsApp.use(express.methodOverride());
+          httpsApp.use(express.compress());
+          httpsApp.use(express.static('../WebContent'));
+          httpsApp.use(allowCrossDomain);
+          httpsApp.use(express.cookieParser());
+        });
+
+        httpsApp.configure('development', function(){
+          httpsApp.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+        });
+
+        httpsApp.configure('production', function(){
+          httpsApp.use(express.errorHandler());
+        });
+
 	
-	var rtr = new Router(app);
-	
+	var rtr = new Router(httpApp);
+	var httpsRtr = new Router(httpsApp);
+
 
 	rtr.load();
 	rtr.setup();
+
+	httpsRtr.load();
+	httpsRtr.setup();	
 	
-	
-	app.get('/api/node/recycle', function(req, res) {
+	httpApp.get('/api/node/recycle', function(req, res) {
 	
 		if (req.query != null && req.query.token != null) {
 			UserAuth.isInRole(req.query.token, 'Administrators', function(err, hasRole) {
@@ -122,6 +176,28 @@ if (cluster.isMaster) {
 		
 	});
 	
+	 httpsApp.get('/api/node/recycle', function(req, res) {
+
+                if (req.query != null && req.query.token != null) {
+                        UserAuth.isInRole(req.query.token, 'Administrators', function(err, hasRole) {
+                                if (err != null || !hasRole) {
+                                        res.json({ status: 'Not Authorized' }, 200);
+                                }
+
+                                console.log("Express server recycling from api call.");
+
+                                var response = { status: 'ok' };
+                                res.json(response, 200);
+                                setTimeout(function() { process.send({ cmd: 'clusterRecycle' }); }, 1250);
+                        });
+                } else {
+                        res.json({ status: 'Not Authorized' }, 200);
+                        return;
+                }
+
+
+        });
+
 	
 	
 	/*
@@ -149,7 +225,11 @@ if (cluster.isMaster) {
 	});
 	*/
 	
-	app.listen(80, '10.1.224.64', function(){
-	  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+	httpApp.listen(80, '10.1.224.64', function(){
+	  console.log("Express server listening on port %d in %s mode", httpApp.address().port, httpApp.settings.env);
+	});
+
+	httpsApp.listen(443, '10.1.224.64', function (){
+	 console.log("Express server listening on port %d in %s mode", httpsApp.address().port, httpsApp.settings.env);
 	});
 }
